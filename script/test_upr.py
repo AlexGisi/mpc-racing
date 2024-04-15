a8bfa869b44ad164f4a25e1bf2027773ebda8b7a
Alex Gisi <apgisi11@gmail.com>
2024-04-15 14:50:07 -0500

    Add animation for whole track

--- script/test_upr.py
+++ script/test_upr.py
@@ -10,33 +10,48 @@ from matplotlib import pyplot as plt
 import numpy as np
 from splines.ParameterizedCenterline import ParameterizedCenterline
 
+# Assuming the ParameterizedCenterline and its methods are correctly defined and imported.
 cl = ParameterizedCenterline()
 cl.from_file("waypoints/shanghai_intl_circuit")
 
-S = 460
-sx, sy = cl.Gx(S), cl.Gy(S)
+def at_point(S, ax):
+    sx, sy = cl.Gx(S), cl.Gy(S)
 
-upr = cl.unit_principal_normal(S)
-upr_dx, upr_dy = upr[0]*10, upr[1]*10
+    upr = cl.unit_principal_normal(S)
+    upr_dx, upr_dy = upr[0]*10, upr[1]*10
 
-upr1 = cl.unit_principal_normal1(S)
-upr1_dx, upr1_dy = upr1[0]*5, upr1[1]*5
+    upr1 = cl.unit_principal_normal1(S)
+    upr1_dx, upr1_dy = upr1[0]*5, upr1[1]*5
 
-tn = cl.unit_tangent(S)
-tn_dx, tn_dy = tn[0]*10, tn[1]*10
+    tn = cl.unit_tangent(S)
+    tn_dx, tn_dy = tn[0]*10, tn[1]*10
 
-track_plot_points = list(np.linspace(S-30, S+30, 10000))
-track_x = [cl.Gx(s) for s in track_plot_points]
-track_y = [cl.Gy(s) for s in track_plot_points]
+    track_plot_points = list(np.linspace(S-30, S+30, 1000))
+    track_x = [cl.Gx(s) for s in track_plot_points]
+    track_y = [cl.Gy(s) for s in track_plot_points]
 
-plt.plot(track_x, track_y)
+    # Clear the previous arrows and plot
+    ax.cla() 
+    ax.plot(track_x, track_y)
 
-plt.arrow(sx, sy, upr_dx, upr_dy, width=0.05, head_length=2)
+    ax.arrow(sx, sy, upr_dx, upr_dy, width=0.05, head_length=2)
+    ax.arrow(sx, sy, upr1_dx, upr1_dy, width=0.05, head_length=2, color="red")
+    ax.arrow(sx, sy, tn_dx, tn_dy, width=0.05, head_length=2)
+    
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_xlim([min(track_x)-10, max(track_x)+10])
+    ax.set_ylim([min(track_y)-10, max(track_y)+10])
 
-# Note how this one is sometimes good, sometimes bad, sometimes horrific.
-plt.arrow(sx, sy, upr1_dx, upr1_dy, width=0.05, head_length=2, color="red")
+def all(cl):
+    plt.ion() 
+    fig, ax = plt.subplots()
+    points = list(np.linspace(0.1, cl.length-0.1, 1000))
+    for s in points:
+        at_point(s, ax)
+        plt.draw()
+        plt.pause(0.01) 
 
-plt.arrow(sx, sy, tn_dx, tn_dy, width=0.05, head_length=2)
-plt.gca().set_aspect('equal', adjustable='box')
+    plt.ioff() 
+    plt.show()
 
-plt.show()
+all(cl)
